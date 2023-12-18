64f621f25ef01a64d261af07997ba39789d63dac
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-17 22:25:18 -0600

    add pinyin puzzle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,25 @@
 
 <h2>2023</h2>
 <ul>
-    <li><a href="2023/12-days-of-christmas">12 days of Christmas</a></li>
-    <li><a href="2023/unfolded-dodecahedrons">Unfolded dodecahedrons</a></li>
-    <li><a href="2023/gift-delivery">Gift delivery</a></li>
-    <li><a href="2023/pinyin-words">Pinyin words</a></li>
+    <li><a href="2023/12-days-of-christmas">12 days of Christmas</a>
+    <ul>
+        <li>Part 1: Not solved yet.</li>
+        <li>Part 2: Not solved yet.</li>
+    </ul>
+</li>
+    <li><a href="2023/unfolded-dodecahedrons">Unfolded dodecahedrons</a>
+        <ul>
+            <li>Part 1: Not solved yet.</li>
+            <li>Part 2: Not solved yet.</li>
+        </ul></li>
+    <li><a href="2023/gift-delivery">Gift delivery</a>
+        <ul>
+            <li>Part 1: Not solved yet.</li>
+            <li>Part 2: Not solved yet.</li>
+        </ul></li>
+    <li><a href="2023/pinyin-words">Pinyin words</a>
+        <ul>
+            <li>Part 1: Not solved yet.</li>
+            <li>Part 2: Not solved yet.</li>
+        </ul></li>
 </ul>
--- src/routes/2023/pinyin-words/+page.svelte
+++ src/routes/2023/pinyin-words/+page.svelte
@@ -1,1 +1,76 @@
-pinyin
+<pre>
+In Hanyu Pinyin, there are 22 possible syllable initials:
+
+b, p, m, f
+d, t, n, l
+g, k, h
+j, q, x
+zh, ch, sh, r
+z, c, s
+∅ (null initial)
+
+and 35 possible syllable finals:
+
+Group a: a,e,ai,ei,ao,ou,an,en,ang,eng,ong
+Group i: ia,ie,iao,iu,ian,in,iang,ing,iong
+Group u: ua,uo,uai,ui,uan,un,uang
+Group ü: ü,üe,üan,ün
+Misfits: i, o, u, er
+
+Syllables can be formed by combining an initial with a final. However, not all possible combinations are phonotactically valid.
+
+The rules below form a simplified approximation of the rules of Mandarin Chinese phonotactics and pinyin spelling:
+<ol><li>∅ (the null initial) can be combined with any of the finals. However, some finals are written differently in a syllable with a null initial. 
+Namely:
+<table>
+    <tr>
+        <td>∅+i=yi
+∅+ia=ya
+∅+iao=yao
+∅+ie=ye
+∅+iu=you
+∅+ian=yan
+∅+in=yin
+∅+iang=yang
+∅+ing=ying
+∅+iong=yong</td>
+        <td>∅+u=wu
+∅+ua=wa
+∅+uo=wo
+∅+uai=wai
+∅+ui=wei
+∅+uan=wan
+∅+un=wen
+∅+uang=wang</td>
+        <td>∅+ü=yu
+∅+üe=yue
+∅+üan=yuan
+∅+ün=yun</td>
+    </tr>
+</table>
+</li><li>"Group a" finals can be paired with any initial except j,q,x</li>
+<li>"Group i" finals can only be paired with ∅,b,p,m,d,t,n,l,j,q,x </li>
+<li>"Group u" finals can only be paired with ∅,d,t,n,l,g,k,h,zh,ch,sh,r,z,c,s</li>
+<li>"Group ü" finals can only be paired with ∅,n,l,j,q,x. 
+When j,q,x are paired with ü, the diacritics are dropped. E.g. jü &rAarr; ju</li>
+<li>"i" can be paired with any initial except g,k,h</li>
+<li>"o" can only be paired with ∅,b,p,m,f,l</li>
+<li>"u" can be paired with any initial except j,q,x</li>
+<li>"er" can only be paired with ∅</li></ol>
+<h3>Problem 1</h3>According to the rules above, how many possible syllables are there in Hanyu Pinyin?
+<h3>Problem 2</h3>Some words in English can be reanalyzed as a sequence of pinyin syllables. 
+
+For example, "enchilada" can be divided into valid pinyin syllables en-chi-la-da.
+
+However, some English words have combinations of letters that cannot be divided into valid pinyin syllables, such as "chocolate".
+
+What is the longest word in the English language (according to <a href="https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt">this wordlist</a>) that can be reanalyzed as a series of pinyin syllables?
+
+If there are multiple longest words, break the tie by choosing the word that comes first alphabetically.
+</pre>
+
+<style>
+    td {
+        vertical-align: top;
+    }
+</style>

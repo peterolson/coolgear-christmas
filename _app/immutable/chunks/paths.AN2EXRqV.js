82f95f693484ad78b283c12492475ee07f87a57c
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-18 15:04:45 -0600

    Updates

---
+++ _app/immutable/chunks/paths.AN2EXRqV.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1w7xcvw?.base??"/coolgear-christmas",a=globalThis.__sveltekit_1w7xcvw?.assets??s;export{a,s as b};
---
+++ _app/immutable/nodes/7.XUdMec-3.js
@@ -0,0 +1,34 @@
+import{s,n as o}from"../chunks/scheduler.k-kUyWhY.js";import{S as l,i,g as h,h as c,y as f,a as u,f as m}from"../chunks/index.5XCitCbI.js";import{b as a}from"../chunks/paths.AN2EXRqV.js";function g(r){let e,n=`Without a doubt, the most beautiful ornament to put at the top of a Christmas tree is a stellated dodecahedron. It is of upmost importance to <b>Cool</b><i>Gear</i>&#39;s bottom line to develop the most efficient possible process to manufacture these ornaments. 
+<img src="${a}/img/stellated_dodecahedron.png" alt="stellated dodecahedron"/>
+But let&#39;s not get too far ahead ourselves -- before we can make stellated dodecahedrons, we first need to nail down the best way to make simple dodecahedrons. 
+
+One easy way to mass-produce dodecahedrons is to cut out a flat unfolded shape from cardboard, and then fold it into a dodecahedron.
+
+<h3>Problem 1</h3>How many unique ways are there to unfold a dodecahedron onto a flat surface so that all of the faces stay connected and do not overlap? Reflected and rotated versions of unfoldings do not contribute to the total count.
+
+Here is an example of one possible way to unfold a dodecahedron:
+
+<img src="${a}/img/unfolded_dodecahedron.png" alt="dodecahedron unfolded"/>
+
+For reference, if we were considering tetrahedrons, rather than dodecahedrons, the answer would be 2.
+Similarly, if we were considering cubes, the answer would be 11.
+ 
+
+<h3>Problem 2</h3>Suppose that one edge of a dodecahedron has a length of <b>1</b>. 
+
+We want to find the smallest bounding rectangle, i.e. the unfolding of a dodecahedron that can be cut out of the smallest possible rectangular sheet of cardboard.
+
+For reference, the smallest bounding rectangle for a tetrahedron has an area of <b>5*sqrt(3)/4 ≈ 2.165</b>:
+
+<img src="${a}/img/tetrahedron_min_area.png" alt="tetrahedron smallest bounding rectangle"/>
+
+We also want to find the largest bounding rectangle, i.e. the unfolding of a dodecahedron that requires the largest possible rectangular sheet of cardboard.
+
+For reference, the largest bounding rectangle for a tetrahedron has an area of <b>2*sqrt(3) ≈ 3.464</b>:
+
+<img src="${a}/img/tetrahedron_max_area.png" alt="tetrahedron largest bounding rectangle"/>
+
+What is the difference in area between the smallest and largest bounding rectangles for a dodecahedron? (Round your answer to three decimal places.)
+
+For reference, the answer would be <b>1.299</b> if we were considering tetrahedrons, rather than dodecahedrons.
+`;return{c(){e=h("pre"),e.innerHTML=n},l(t){e=c(t,"PRE",{"data-svelte-h":!0}),f(e)!=="svelte-1p8w9h7"&&(e.innerHTML=n)},m(t,d){u(t,e,d)},p:o,i:o,o,d(t){t&&m(e)}}}class _ extends l{constructor(e){super(),i(this,e,null,g,s,{})}}export{_ as component};

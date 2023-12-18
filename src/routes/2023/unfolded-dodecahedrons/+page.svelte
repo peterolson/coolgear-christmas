c48ad593ec8b6150f2aabebc1271d8fdd69352fc
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-18 15:04:22 -0600

    add examples

--- src/routes/2023/unfolded-dodecahedrons/+page.svelte
+++ src/routes/2023/unfolded-dodecahedrons/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-    import { base } from '$app/paths';
+	import { base } from '$app/paths';
 </script>
+
 <pre>
-Without a doubt, the most beautiful ornament to put at the top of a Christmas tree is a stellated dodecahedron. It is of upmost importance to <b>Cool</b><i>Gear</i>'s bottom line to develop the most efficient possible process to manufacture these ornaments. 
-<img src="{base}/img/stellated_dodecahedron.png" alt="stellated dodecahedron">
+Without a doubt, the most beautiful ornament to put at the top of a Christmas tree is a stellated dodecahedron. It is of upmost importance to <b
+		>Cool</b
+	><i>Gear</i
+	>'s bottom line to develop the most efficient possible process to manufacture these ornaments. 
+<img src="{base}/img/stellated_dodecahedron.png" alt="stellated dodecahedron" />
 But let's not get too far ahead ourselves -- before we can make stellated dodecahedrons, we first need to nail down the best way to make simple dodecahedrons. 
 
 One easy way to mass-produce dodecahedrons is to cut out a flat unfolded shape from cardboard, and then fold it into a dodecahedron.
@@ -12,7 +16,7 @@ One easy way to mass-produce dodecahedrons is to cut out a flat unfolded shape f
 
 Here is an example of one possible way to unfold a dodecahedron:
 
-<img src="{base}/img/unfolded_dodecahedron.png" alt="dodecahedron unfolded">
+<img src="{base}/img/unfolded_dodecahedron.png" alt="dodecahedron unfolded" />
 
 For reference, if we were considering tetrahedrons, rather than dodecahedrons, the answer would be 2.
 Similarly, if we were considering cubes, the answer would be 11.
@@ -20,7 +24,24 @@ Similarly, if we were considering cubes, the answer would be 11.
 
 <h3>Problem 2</h3>Suppose that one edge of a dodecahedron has a length of <b>1</b>. 
 
-We want to find the unfolding of a dodecahedron that can be cut out of the smallest possible rectangular sheet of cardboard.
+We want to find the smallest bounding rectangle, i.e. the unfolding of a dodecahedron that can be cut out of the smallest possible rectangular sheet of cardboard.
 
-What is the area of this sheet of cardboard? (Round to the nearest three digits).
+For reference, the smallest bounding rectangle for a tetrahedron has an area of <b
+		>5*sqrt(3)/4 ≈ 2.165</b
+	>:
+
+<img src="{base}/img/tetrahedron_min_area.png" alt="tetrahedron smallest bounding rectangle" />
+
+We also want to find the largest bounding rectangle, i.e. the unfolding of a dodecahedron that requires the largest possible rectangular sheet of cardboard.
+
+For reference, the largest bounding rectangle for a tetrahedron has an area of <b
+		>2*sqrt(3) ≈ 3.464</b
+	>:
+
+<img src="{base}/img/tetrahedron_max_area.png" alt="tetrahedron largest bounding rectangle" />
+
+What is the difference in area between the smallest and largest bounding rectangles for a dodecahedron? (Round your answer to three decimal places.)
+
+For reference, the answer would be <b>1.299</b
+	> if we were considering tetrahedrons, rather than dodecahedrons.
 </pre>

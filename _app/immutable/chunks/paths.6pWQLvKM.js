7ea219f116cc632fee7a329d5aa76a3d5a98906d
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-30 20:36:06 -0600

    Updates

---
+++ _app/immutable/chunks/paths.6pWQLvKM.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_41x67y?.base??"/coolgear-christmas",a=globalThis.__sveltekit_41x67y?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.msm8W_TX.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_rs6v9u?.base??"/coolgear-christmas",a=globalThis.__sveltekit_rs6v9u?.assets??s;export{a,s as b};

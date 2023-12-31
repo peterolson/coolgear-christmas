5f837e01cdfe88adb035a56db35c2fa762dbac13
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-30 19:45:05 -0600

    add solution

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,37 @@
 <script lang="ts">
-    import { base } from '$app/paths';
+	import { base } from '$app/paths';
 </script>
 
 <h1><b>Cool</b><i>Gear</i> Christmas Problems</h1>
 
 <h2>2023</h2>
 <ul>
-    <li><a href="{base}/2023/12-days-of-christmas">12 days of Christmas</a>
-    <ul>
-        <li>Part 1: ✅ Solved by Alexis on 2023-12-23</li>
-        <li>Part 2: ✅ Solved by Alexis on 2023-12-23</li>
-    </ul>
-</li>
-    <li><a href="{base}/2023/unfolded-dodecahedrons">Unfolded dodecahedrons</a>
-        <ul>
-            <li>Part 1: Not solved yet.</li>
-            <li>Part 2: Not solved yet.</li>
-        </ul></li>
-    <li><a href="{base}/2023/gift-delivery">Gift delivery</a>
-        <ul>
-            <li>Part 1: Not solved yet.</li>
-            <li>Part 2: Not solved yet.</li>
-        </ul></li>
-    <li><a href="{base}/2023/pinyin-words">Pinyin words</a>
-        <ul>
-            <li>Part 1: Not solved yet.</li>
-            <li>Part 2: Not solved yet.</li>
-        </ul></li>
+	<li>
+		<a href="{base}/2023/12-days-of-christmas">12 days of Christmas</a>
+		<ul>
+			<li>Part 1: ✅ Solved by ChatGPT on 2023-12-23</li>
+			<li>Part 2: ✅ Solved by ChatGPT on 2023-12-23</li>
+		</ul>
+	</li>
+	<li>
+		<a href="{base}/2023/unfolded-dodecahedrons">Unfolded dodecahedrons</a>
+		<ul>
+			<li>Part 1: Not solved yet.</li>
+			<li>Part 2: Not solved yet.</li>
+		</ul>
+	</li>
+	<li>
+		<a href="{base}/2023/gift-delivery">Gift delivery</a>
+		<ul>
+			<li>Part 1: Solved by Jerry on 2023-12-30.</li>
+			<li>Part 2: Solved by Jerry on 2023-12-30.</li>
+		</ul>
+	</li>
+	<li>
+		<a href="{base}/2023/pinyin-words">Pinyin words</a>
+		<ul>
+			<li>Part 1: Not solved yet.</li>
+			<li>Part 2: Not solved yet.</li>
+		</ul>
+	</li>
 </ul>
